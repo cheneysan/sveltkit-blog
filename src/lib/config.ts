94880431247935ed63875cc0ca12f737f5ceae8b
rcheney <rcheney@p3s.dev>
2023-09-05 02:04:59 +0100

    Initial config file and layout

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import {dev} from '$app/environment'
+
+export const title = 'SvelteKit blog test'
+export const description = 'SvelteKit blog test'
+export const url = dev ? 'http://localhost:5173' : 'https://replace.me'
